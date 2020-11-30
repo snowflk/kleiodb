9c7bb83f695dd43939720c93f7d9a75fea9fc22b
Phuc Tran <tran@silpion.de>
2020-11-30 23:06:33 +0100

    changed interfaces

--- internal/persistence/snapshot_keeper.go
+++ internal/persistence/snapshot_keeper.go
@@ -3,18 +3,18 @@ package persistence
 import "time"
 
 type SnapshotKeeper interface {
-	SaveSnapshot(source, key string, data []byte) error
+	SaveSnapshot(source, name string, data []byte) error
 
-	GetSnapshot(source, key string) (RawSnapshot, error)
+	GetSnapshot(source, name string) (RawSnapshot, error)
 
 	// FindSnapshots finds all snapshots matching the given pattern from a source
-	// Returns an array of snapshot keys
+	// Returns an array of snapshot names
 	FindSnapshots(source string, pattern Pattern) ([]string, error)
 	Close() error
 }
 type RawSnapshot struct {
 	Source    string
-	Key       string
+	Name      string
 	Timestamp time.Time
 	Payload   []byte
 }
