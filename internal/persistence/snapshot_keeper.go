964143c85a98e8b27951832d0827e9d1e1dc5688
Phuc Tran <tran@silpion.de>
2020-12-02 18:07:35 +0100

    changed interfaces

--- internal/persistence/snapshot_keeper.go
+++ internal/persistence/snapshot_keeper.go
@@ -2,9 +2,16 @@ package persistence
 
 import "time"
 
+// SnapshotKeeper is a storage for snapshots
+// A "source" is a stream or a view the snapshot is based on
+// A snapshot belongs to a single source and has a unique name
 type SnapshotKeeper interface {
+	// SaveSnapshot saves snapshot that belongs to the given source.
+	// If the name already exists, the data will be overwritten
 	SaveSnapshot(source, name string, data []byte) error
 
+	// GetSnapshot retrieve the snapshot from the given source
+	// If the source or the name cannot be found, an error will be returned
 	GetSnapshot(source, name string) (RawSnapshot, error)
 
 	// FindSnapshots finds all snapshots matching the given pattern from a source
@@ -12,6 +19,8 @@ type SnapshotKeeper interface {
 	FindSnapshots(source string, pattern Pattern) ([]string, error)
 	Close() error
 }
+
+// RawSnapshot contains metadata and payload of the snapshot
 type RawSnapshot struct {
 	Source    string
 	Name      string
---
+++ internal/persistence/storage.go
@@ -0,0 +1,14 @@
+package persistence
+
+// Storage is the interface to low-level storage module.
+// The storage must be able to store events with global ordering and local ordering in each views
+// EventKeeper contains all operations on events and views ( streams and custom views on higher level module)
+// ViewKeeper contains all operations on view metadata
+// SnapshotKeeper contains all operations on snapshots
+// The implementation of this interface must use the package "testsuite" for unit-testing, so that we can make sure
+// the implementation satisfies all requirements
+type Storage interface {
+	EventKeeper
+	ViewKeeper
+	SnapshotKeeper
+}
